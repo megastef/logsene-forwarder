66c07bc554da9c4583aa3528c397c0a14f3f32f4
Stefan Thies <s.thies@bigdata-analyst.de>
2015-07-15 11:22:17 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-	
+# Expect ENV var LOGSENE_TOKEN !!!
+
 if [ $1 == "service" ]; then
 	# TCP Input lines
 	export TCP_LINE_INPUT="input://tcp://0.0.0.0:10000"
@@ -17,18 +18,16 @@ if [ $1 == "service" ]; then
 	export TCP_JSON_FILTER="filter://bunyan://"
 	export TCP_JSON_FILTER2="filter://compute_field://message?value=#{msg} filter://compute_field://@timestamp?value=#{time}"
 
-	echo node-logstash-agent $TCP_JSON_INPUT $TCP_JSON_FILTER $TCP_JSON_FILTER2 $LOGSENE_OUTPUT &
 	node-logstash-agent --http_max_sockets 1 $TCP_JSON_INPUT $TCP_JSON_FILTER $TCP_JSON_FILTER2 $LOGSENE_OUTPUT &
 
 	echo listening on UDP 514 for syslog input
 	export SYSLOG_UDP_INPUT="input://udp://0.0.0.0:514?type=syslog"
 	export SYSLOG_FILTERS="filter://regex://syslog?only_type=syslog filter://syslog_pri://?only_type=syslog"
-	echo node-logstash-agent $SYSLOG_UDP_INPUT $SYSLOG_FILTERS $LOGSENE_OUTPUT &
 	node-logstash-agent --http_max_sockets 1  $SYSLOG_TCP_INPUT $SYSLOG_FILTERS $LOGSENE_OUTPUT 
 fi	
 
 if [ $1 == "json" ]; then 
-	node-logstash-agent input://stdin:// $TCP_JSON_FILTER TCP_JSON_FILTER2 $LOGSENE_OUTPUT
+	node-logstash-agent input://stdin:// $TCP_JSON_FILTER $TCP_JSON_FILTER2 $LOGSENE_OUTPUT
 else 
 	node-logstash-agent input://stdin:// $GROK_LINE $LOGSENE_OUTPUT
 fi
